8b2be8ee9eb54721ec1f3140e4d3c5b5ac2027c2
Felipe Cruz <felipe.oliveiracrz@outlook.com>
2022-09-29 19:51:42 -0300

    init commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.subnet_azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
+
+  tags = {
+    Terraform   = "true"
+    Environment = "prod"
+    Name        = "catapimba-corps-vpc"
+    CC          = "10503"
+  }
+}
